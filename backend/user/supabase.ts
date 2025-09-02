8a447bbd495fa9b7869060efbceda6e32922f19f
Leap <git@leap.new>
2025-09-02 00:10:07 +0000

    Manual code change

--- backend/user/supabase.ts
+++ backend/user/supabase.ts
@@ -4,4 +4,7 @@ import { secret } from 'encore.dev/config';
 const supabaseUrl = secret("SupabaseUrl");
 const supabaseKey = secret("SupabaseKey");
 
+console.log("Hello");
+console.log(supabaseUrl());
+
 export const supabase = createClient(supabaseUrl(), supabaseKey());
